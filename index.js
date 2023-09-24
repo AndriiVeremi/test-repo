4e053dd13754e5eeb30a01486d4a169a3a76dacb
Andrii Veremii <dashuk10@gmail.com>
2023-09-24 22:37:27 +0200

    add

--- index.js
+++ index.js
@@ -1,83 +1,16 @@
-const stars = 3;
-let price;
+function checkForSpam(message) {
+    let result;
+    // Change code below this line
+  if (message.toLowerCase().includes("sale") || message.toLowerCase().includes("spam")) {
+    result = true
+  } else {
+    result = false
+  }
+    // Change code above this line
+    return result;
+  }
 
-// if (stars === 1) {
-//   price = 20;
-// } else if (stars === 3) {
-//   price = 40;
-// } else if (stars === 5) {
-//   price = 60;
-// } else {
-//   price = 0;
-// }
-
-// if (stars === 1 || stars === 3) {
-//   price = 20;
-// } else if (stars === 5) {
-//   price = 60;
-// } else {
-//   price = 100;
-// }
-
-// console.log(price);
-
-// switch (stars) {
-//   case 1:
-//     price = 20;
-//     break;
-//   case 2:
-//     price = 30;
-//     break;
-//   case 3:
-//     price = 40;
-//     break;
-// }
-
-// switch (stars) {
-//   case 1:
-//   case 2:
-//     price = 30;
-//     break;
-//   case 3:
-//     price = 40;
-//     break;
-//   case 4:
-//     price = 60;
-//     break;
-// }
-
-// console.log(price);
-
-const options = 2;
-let message;
-
-// switch (options) {
-//   case 1:
-//     message = "зможемо забрати завтра в 12:00";
-//     break;
-//   case 2:
-//     message = "доставлять заказ завтра з 9:00 до 12:00";
-//     break;
-//   case 3:
-//     message = "сьогодні відправим";
-//     break;
-//   default:
-//     message = "вам перезвонить менеджер";
-// }
-
-// if (options === 1) {
-//   message = "зможемо забрати завтра в 12:00";
-// } else if (options === 2) {
-//   message = "доставлять заказ завтра з 9:00 до 12:00";
-// } else if (options === 3) {
-//   message = "сьогодні відправим";
-// } else {
-//   message = "вам перезвонить менеджер";
-// }
-
-console.log(message);
-
-
-for (let i = 0; i < 5; i += 1) {
-    console.log(i)
-}
+  console.log(checkForSpam("Latest technology news"))
+  console.log(checkForSpam("Get best sale offers now!"))
+  
+  
