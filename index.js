5e8ee5812c64931438c3338351d426ee6aac9f7e
Andrii Veremii <dashuk10@gmail.com>
2023-10-06 22:59:28 +0200

    add

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-class Car {}
+class StringBuilder {
+  constructor(initialValue) {
+    this.value = initialValue;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  padEnd(str) {
+    this.value = this.value.concat(str);
+  }
+
+  padStart(str) {
+    this.value = this.value.unshift(str);
+  }
+
+  padBoth(str) {}
+}
+
+// Change code above this line
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
