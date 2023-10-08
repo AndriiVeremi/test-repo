9ed99c8104612e8b6eaf9915f4cf27fb10edd61f
Andrii Veremii <dashuk10@gmail.com>
2023-10-08 20:07:56 +0200

    add

--- index.js
+++ index.js
@@ -1,54 +1,8 @@
-class User {
-  email;
-
-  constructor(email) {
-    this.email = email;
-  }
-
-  get email() {
-    return this.email;
-  }
-
-  set email(newEmail) {
-    this.email = newEmail;
-  }
+function findShort(s) {
+    return s.split(' ').map(item => {
+       console.log(item)
+   })
 }
 
-class Admin extends User {
-  // Change code below this line
-
-  static AccessLevel = {
-    BASIC: "basic",
-    SUPERUSER: "superuser",
-  };
-
-  blacklistedEmails = [];
-
-  constructor({ email, accessLevel }) {
-    super(email);
-    this.accessLevel = accessLevel;
-  }
-
-  blacklist(email) {
-    this.blacklistedEmails.push(email);
-  }
-
-  isBlacklisted(email) {
-    return this.blacklistedEmails.includes(email)
-  }
-
-  // Change code above this line
-}
-
-const mango = new Admin({
-  email: "mango@mail.com",
-  accessLevel: Admin.AccessLevel.SUPERUSER,
-});
-
-console.log(mango.email); // "mango@mail.com"
-console.log(mango.accessLevel); // "superuser"
-
-mango.blacklist("poly@mail.com");
-console.log(mango.blacklistedEmails); // ["poly@mail.com"]
-console.log(mango.isBlacklisted("mango@mail.com")); // false
-console.log(mango.isBlacklisted("poly@mail.com")); // true
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShort("Let's travel abroad shall we"))
