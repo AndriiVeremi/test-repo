6aa7393b8df01947648803981435859aa591437f
Andrii Veremii <dashuk10@gmail.com>
2023-10-08 23:01:06 +0200

    add

--- index.js
+++ index.js
@@ -1,51 +1,11 @@
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
+function squareSum(numbers) {
+  let sum = 0;
 
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  
-  addPotion(newPotion) {
-    for (const obj of this.potions) {
-      if (obj.name === newPotion.name) {
-        return `Error! Potion ${newPotion.name} is already in your inventory!`;
-      }
-    }
-    this.potions.push(newObj);
-    this.potions.push(newPotion);
-  },
+  for (const number of numbers) {
+    sum += number ** 2;
+  }
+  return sum;
+}
 
-  removePotion(potionName) {
-    for (const obj of this.potions) {
-      const potionIndex = this.potions.indexOf(obj);
-      if (obj.name === potionName) {
-        this.potions.splice(potionIndex, 1);
-      } else {
-        return `Potion ${potionName} is not in inventory!`;
-      }
-    }
-  },
-
-  updatePotionName(oldName, newName) {
-    for (const obj of this.potions) {
-      const potionIndex = this.potions.indexOf(obj);
-      if (obj.name === oldName) {
-        this.potions.splice(potionIndex, 1, newName);
-      } else {
-        return `Potion ${oldName} is not in inventory!`;
-      }
-    }
-  },
-};
-
-
-
-console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
-console.log(atTheOldToad.addPotion({ name: "Power potion", price: 270 }));
-console.log(atTheOldToad.removePotion("Dragon breath"));
-console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"));
+console.log(squareSum([1, 2, 2]));
+// console.log(squareSum([0, 3, 4, 5]));
