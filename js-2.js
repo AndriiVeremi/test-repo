2e1f5ef63295763f7f68186ee79a778e2eca0c20
Andrii Veremii <dashuk10@gmail.com>
2023-10-19 20:43:08 +0200

    add

--- js-2.js
+++ js-2.js
@@ -1,7 +1,1 @@
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-  // Change code below this line
-  const total = pricePerDroid * orderedQuantity + deliveryFee;
-  const message = `You ordered droids worth ${total} credits. Delivery (${deliveryFee} credits) is included in total price.`;
-  // Change code above this line
-  return message
-}
+console.log("false" + null);
