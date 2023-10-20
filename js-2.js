fcda4b196690dee9204bd4eac33e0a8255a69df3
Andrii Veremii <dashuk10@gmail.com>
2023-10-20 22:22:26 +0200

    add

--- js-2.js
+++ js-2.js
@@ -1,1 +1,12 @@
-console.log("false" + null);
+function checkAge(age) {
+  let message;
+
+  if () { // Change this line
+    message = 'You are an adult';
+  } else {
+    message = 'You are a minor';
+  }
+
+  return message;
+}
+
